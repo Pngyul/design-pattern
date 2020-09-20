aec8db285b97d85047ac3d9bbf2609f3cd80de6d
Pngyul <Pngyul@163.com>
2020-09-20 22:51:05 +0800

    策略模式

--- src/main/java/com/klayzz/designpattern/DesignPatternApplication.java
+++ src/main/java/com/klayzz/designpattern/DesignPatternApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DesignPatternApplication {
-
+    //https://www.cnblogs.com/bulrush/p/12755279.html
     public static void main(String[] args) {
         SpringApplication.run(DesignPatternApplication.class, args);
     }
