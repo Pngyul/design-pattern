2370b17e5e324587d13ec3d162e6a6938309e8cf
Pngyul <Pngyul@163.com>
2020-10-05 16:40:00 +0800

    添加适配器模式例子readme.md

--- src/main/java/com/klayzz/designpattern/structure/adapter/example/example3/app.java
+++ src/main/java/com/klayzz/designpattern/structure/adapter/example/example3/app.java
@@ -8,6 +8,8 @@ public class app {
         //A系统
         ADemo a = new ADemo(new A());
         //引入B系统
+        // 借助BAdaptor，Demo的代码中，调用IA接口的地方都无需改动，
+        // 只需要将BAdaptor如下注入到Demo即可。
         ADemo b = new ADemo(new BAdaptor(new B()));
     }
 }
